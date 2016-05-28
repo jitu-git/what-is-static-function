f0a4297baf1e0db3b18d94c6d77590ad6863cad2
Jeetendra meena <jitendrameena38@gmail.com>
2016-05-28 13:21:40 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -6,11 +6,41 @@
 class Foo {
 
     /**
+     * static variable
+     *
+     * @var string
+     */
+    public static $static_variable = "STATIC";
+    /**
+     * static method
+     *
      * @method aStaticMethod
      */
     public static function aStaticMethod() {
         echo "Called a static function '<strong>". __FUNCTION__."</strong>' in class". __CLASS__;
     }
 }
+
+/**
+ * Class Bar
+ */
+class Bar extends Foo {
+
+    /**
+     * static method
+     *
+     * @method anotherStatic
+     */
+    public static function anotherStatic(){
+        echo parent::$static_variable;
+        parent::aStaticMethod();
+    }
+}
+
+
 Foo::aStaticMethod();
+echo "</br>";
+printf("Access static variable - %s",Foo::$static_variable);
+echo "</br>";
+Bar::anotherStatic();
 ?>
